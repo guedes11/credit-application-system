4b33f4754e6f78d3dd997629bc45bee828df2d48
Kevem <kevem2010@hotmail.com>
2024-02-20 17:20:30 -0300

    Inserindo data maxima para primeira parcela do Credito

--- src/main/kotlin/dev/kevem/request/credit/system/service/impl/CreditService.kt
+++ src/main/kotlin/dev/kevem/request/credit/system/service/impl/CreditService.kt
@@ -5,11 +5,14 @@ import dev.kevem.request.credit.system.exception.BusinessException
 import dev.kevem.request.credit.system.repository.CreditRepository
 import dev.kevem.request.credit.system.service.ICreditService
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.util.*
 
 @Service
-class CreditService(private val creditRepository: CreditRepository,
-                    private val customerService: CustomerService): ICreditService {
+class CreditService(
+    private val creditRepository: CreditRepository,
+    private val customerService: CustomerService
+) : ICreditService {
     override fun save(credit: Credit): Credit {
         credit.apply {
             customer = customerService.findById(credit.customer?.id!!)
@@ -24,6 +27,11 @@ class CreditService(private val creditRepository: CreditRepository,
     override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
         val credit: Credit = (this.creditRepository.findByCreditCode(creditCode)
             ?: throw RuntimeException("Creditcode $creditCode not found"))
-        return if(credit.customer?.id == customerId) credit else throw BusinessException("Contact Admin")
+        return if (credit.customer?.id == customerId) credit else throw BusinessException("Contact Admin")
+    }
+
+    private fun validDayFirstInstallment(dayFirstInstallment: LocalDate): Boolean {
+        return if (dayFirstInstallment.isBefore(LocalDate.now().plusMonths(3))) true
+        else throw BusinessException("Data Inválida.")
     }
 }
